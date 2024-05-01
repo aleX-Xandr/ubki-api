2c53d9e0f07b012a24137899aa5851447f3e21e3
aleX-Xandr <72668137+aleX-Xandr@users.noreply.github.com>
2024-05-01 23:01:49 +0300

    remove outdated files

--- src/config.py
+++
@@ -1,6 +0,0 @@
-from fastapi.templating import Jinja2Templates
-
-WORK_DIR = "src/"
-TEMPLATES = Jinja2Templates(directory=f"{WORK_DIR}templates")
-BASE_URL = "https://test.ubki.ua" # "https://secure.ubki.ua"
-DOMAIN = "localhost"
--- src/models/data.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class Data(BaseModel):
-    data: dict
-    reqtype: str = "i"
-    reqidout: str = ""
-    reqreason: str = "0"
-    delreason: str = ""
